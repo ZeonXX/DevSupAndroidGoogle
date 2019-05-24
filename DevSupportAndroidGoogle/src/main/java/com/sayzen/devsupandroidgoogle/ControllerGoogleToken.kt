26946703a691a89e06c4eb2d63039bda7f490a3e
Холин Дмитрий <kholin@beward.ru>
2019-05-24 15:54:14 +0700

    Commit

--- DevSupportAndroidGoogle/src/main/java/com/sayzen/devsupandroidgoogle/ControllerGoogleToken.kt
+++ DevSupportAndroidGoogle/src/main/java/com/sayzen/devsupandroidgoogle/ControllerGoogleToken.kt
@@ -1,7 +1,15 @@
 package com.sayzen.devsupandroidgoogle
 
+import android.view.Gravity
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
+import com.sup.dev.android.app.SupAndroid
+import com.sup.dev.android.tools.ToolsIntent
 import com.sup.dev.java.libs.api_simple.client.TokenProvider
+import com.sup.dev.java.tools.ToolsThreads
+import java.util.concurrent.TimeUnit
 
 object ControllerGoogleToken {
 
@@ -40,8 +48,14 @@ object ControllerGoogleToken {
     }
 
     fun logout(callback: (()->Unit)) {
-        googleAccount = null
-        GoogleAuth.exit(callback)
+        getClient { googleApiClient ->
+            ToolsThreads.main {
+                googleAccount = null
+                Auth.GoogleSignInApi.signOut(googleApiClient)
+                callback.invoke()
+                googleApiClient.disconnect()
+            }
+        }
     }
 
     fun getToken(onResult: (String?)->Unit) {
@@ -49,7 +63,7 @@ object ControllerGoogleToken {
             onResult.invoke(googleAccount!!.idToken)
             return
         }
-        GoogleAuth.getGoogleToken { googleAccount ->
+        getGoogleToken { googleAccount ->
             ControllerGoogleToken.googleAccount = googleAccount
             onResult.invoke(googleAccount?.idToken)
         }
@@ -66,5 +80,55 @@ object ControllerGoogleToken {
     fun containsToken(): Boolean {
         return googleAccount != null
     }
+
+    //
+    //  Autch
+    //
+
+    fun getGoogleToken(onComplete: (GoogleSignInAccount?) -> Unit) {
+        getClient { googleApiClient ->
+            val googleSignInResult = Auth.GoogleSignInApi.silentSignIn(googleApiClient).await(500, TimeUnit.MILLISECONDS)
+
+            if (googleSignInResult.isSuccess && googleSignInResult.signInAccount != null) {
+                ToolsThreads.main { onComplete.invoke(googleSignInResult.signInAccount) }
+                googleApiClient.disconnect()
+            } else {
+
+                ToolsIntent.startIntentForResult(Auth.GoogleSignInApi.getSignInIntent(googleApiClient)) { resultCode, intent ->
+                    val result = Auth.GoogleSignInApi.getSignInResultFromIntent(intent)
+                    if (resultCode == 0 || result == null || !result.isSuccess) {
+                        onComplete.invoke(null)
+                    } else
+                        onComplete.invoke(result.signInAccount)
+
+                    googleApiClient.disconnect()
+                }
+            }
+        }
+    }
+
+    //
+    //  Support
+    //
+
+
+    private fun getClient(onConnect: (GoogleApiClient) -> Unit) {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("276237287601-6e9aoah4uivbjh6lnn1l9hna6taljd9u.apps.googleusercontent.com")
+                .build()
+
+        val client = GoogleApiClient.Builder(SupAndroid.activity!!)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .setGravityForPopups(Gravity.BOTTOM or Gravity.CENTER)
+                .build()
+
+        ToolsThreads.thread {
+            client.blockingConnect()
+            onConnect.invoke(client)
+        }
+
+    }
+
 
 }
