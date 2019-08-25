c4e6b6646ba967f017ab35ed8b289f4a262a5e0e
zeooondevel <zeooondevel@gmail.com>
2019-08-25 10:46:59 +0700

    Commit

---
+++ src/main/java/com/sayzen/devsupandroidgoogle/ControllerAdsFullscreen.kt
@@ -0,0 +1,86 @@
+package com.sayzen.devsupandroidgoogle
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import com.google.ads.consent.ConsentInformation
+import com.google.ads.consent.ConsentStatus
+import com.google.ads.mediation.admob.AdMobAdapter
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import com.sup.dev.android.app.SupAndroid
+import com.sup.dev.android.tools.ToolsAndroid
+import com.sup.dev.java.libs.debug.err
+import com.sup.dev.java.tools.ToolsThreads
+
+object ControllerAdsFullscreen {
+
+    private var ad: InterstitialAd? = null
+    private var lastShow = System.currentTimeMillis()
+    private var key_ads = arrayOf(ControllerAds.DEBUG_AD)
+    private var keyAd = 0
+
+    fun init(key_ads: Array<String>) {
+        this.key_ads = if (ControllerAds.isDebug) arrayOf(ControllerAds.DEBUG_AD) else key_ads
+        loadAd(true)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun loadAd(force: Boolean = false) {
+        if (ad != null && ad!!.isLoading) return
+        if (!force && !ToolsAndroid.appIsVisible()) return
+
+        var key_ad = ""
+        if (!ControllerAds.isDebug) {
+            keyAd++
+            if (keyAd >= key_ads.size) keyAd = 0
+            key_ad = key_ads[keyAd]
+        }
+
+        ad = InterstitialAd(SupAndroid.appContext!!)
+        ad!!.adUnitId = key_ad
+        ad!!.adListener = object : AdListener() {
+
+            override fun onAdLoaded() {
+                err("XX onAdLoaded")
+            }
+
+            override fun onAdFailedToLoad(i: Int) {
+                err("XX onAdFailedToLoad $i")
+                ToolsThreads.main(1000 * 10) { loadAd() }
+            }
+
+            override fun onAdClosed() {
+                loadAd()
+            }
+
+        }
+
+
+        val extras = Bundle()
+        extras.putString("max_ad_content_rating", "T")
+        if (ConsentInformation.getInstance(SupAndroid.appContext).isRequestLocationInEeaOrUnknown && ControllerAdsConsent.status != ConsentStatus.PERSONALIZED) {
+            extras.putString("npa", "1")
+        }
+        MobileAds.initialize(SupAndroid.appContext, ControllerAds.key_app)
+
+
+        ad!!.loadAd(AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter::class.java, extras).build())
+    }
+
+
+    fun showIfNeed() {
+        if (System.currentTimeMillis() <= lastShow + 1000 * 60 * 3) return
+
+        if (ad!!.isLoaded && SupAndroid.activityIsVisible) {
+            ad!!.show()
+            lastShow = System.currentTimeMillis()
+        } else {
+            if (!ad!!.isLoading) loadAd()
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/sayzen/devsupandroidgoogle/ControllerAdsNative.kt
@@ -0,0 +1,52 @@
+package com.sayzen.devsupandroidgoogle
+
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdLoader
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.formats.NativeAdOptions
+import com.google.android.gms.ads.formats.UnifiedNativeAd
+import com.sup.dev.android.app.SupAndroid
+import com.sup.dev.android.tools.ToolsAndroid
+import com.sup.dev.java.libs.debug.log
+
+object ControllerAdsNative {
+
+    private var adLoader: AdLoader? = null
+    private var key_ads = arrayOf(ControllerAds.DEBUG_AD)
+    private var keyAd = 0
+
+    fun init(){
+        this.key_ads = if (ControllerAds.isDebug) arrayOf(ControllerAds.DEBUG_AD) else key_ads
+        loadAd(true)
+    }
+
+    fun loadAd(force: Boolean = false){
+        if (adLoader != null && adLoader!!.isLoading) return
+        if (!force && !ToolsAndroid.appIsVisible()) return
+
+        var key_ad = ""
+        if (!ControllerAds.isDebug) {
+            keyAd++
+            if (keyAd >= key_ads.size) keyAd = 0
+            key_ad = key_ads[keyAd]
+        }
+
+        adLoader = AdLoader.Builder(SupAndroid.appContext, key_ad)
+                .forUnifiedNativeAd { ad : UnifiedNativeAd ->
+                    log("Ad is loaded " + ad)
+                    // Show the ad.
+                }
+                .withAdListener(object : AdListener() {
+                    override fun onAdFailedToLoad(errorCode: Int) {
+                        log("onAdFailedToLoad " + errorCode)
+                        // Handle the failure by logging, altering the UI, and so on.
+                    }
+                })
+                .withNativeAdOptions(NativeAdOptions.Builder()
+                        // Methods in the NativeAdOptions.Builder class can be
+                        // used here to specify individual options settings.
+                        .build())
+                .build()
+    }
+
+}
