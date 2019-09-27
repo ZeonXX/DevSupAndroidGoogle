3e6492d14a836f594d90922cbbc86171ee696fd4
Холин Дмитрий <kholin@beward.ru>
2019-09-27 17:33:42 +0700

    Commit

--- src/main/java/com/sayzen/devsupandroidgoogle/ControllerFirebaseAnalytics.kt
+++ src/main/java/com/sayzen/devsupandroidgoogle/ControllerFirebaseAnalytics.kt
@@ -1,12 +1,17 @@
 package com.sayzen.devsupandroidgoogle
 
+import android.app.Activity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.sup.dev.android.app.SupAndroid
 
 object ControllerFirebaseAnalytics {
 
-    private val firebaseAnalytics = FirebaseAnalytics.getInstance(SupAndroid.appContext!!)
+    private var firebaseAnalytics:FirebaseAnalytics? = null
+
+    fun init(activity:Activity){
+        firebaseAnalytics = FirebaseAnalytics.getInstance(activity)
+    }
 
     fun post_1(){
         val bundle = Bundle()
@@ -16,7 +21,7 @@ object ControllerFirebaseAnalytics {
         bundle.putString(FirebaseAnalytics.Param.ITEM_BRAND, "brand_plus_clicked")
         bundle.putString(FirebaseAnalytics.Param.ITEM_LOCATION_ID, "location_id_plus_clicked")
         bundle.putString(FirebaseAnalytics.Param.ITEM_VARIANT, "variant_id_plus_clicked")
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        firebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 
     fun post_2(){
@@ -27,7 +32,7 @@ object ControllerFirebaseAnalytics {
         bundle.putString(FirebaseAnalytics.Param.ITEM_BRAND, "brand_search_clicked")
         bundle.putString(FirebaseAnalytics.Param.ITEM_LOCATION_ID, "location_id_search_clicked")
         bundle.putString(FirebaseAnalytics.Param.ITEM_VARIANT, "variant_search_clicked")
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        firebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 
 
